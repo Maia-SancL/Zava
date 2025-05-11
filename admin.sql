692add8268d5482dfa5c8f28b407c71e0b0daa4d
juanchune <mobiliajuanmartin@gmail.com>
2025-05-11 15:08:27 -0300

    base de datos
    creacion de la bd, tablas, insertando roles y la cuenta de admin

--- admin.sql
+++ admin.sql
@@ -1,7 +1,2 @@
-INSERT INTO Roles (nombre, descripcion) VALUES
-('Administrador', 'Dar de baja productos, recetas, usuarios y negocios'),
-('Usuario', 'Comentar y calificar restaurantes, productos y recetas, comprar productos y crear recetas'),
-('Vendedor', 'Crear productos y publicar su restaurante')
-
 INSERT INTO Usuarios (nombre, apellido, nickname, correo, telefono, contrasenia, rol, foto) VALUES
-('Masha', 'Sánchez', 'MASHARRRDA', 'sanchezmaialen6@gmail.com', '2262634225', '1234', '0', 'perfil.png')
+('Masha', 'Sánchez', 'MASHARRRDA', 'sanchezmaialen6@gmail.com', '2262634225', '1234', '1', 'perfil.png');
--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,35 +1,37 @@
 CREATE DATABASE Zava;
+USE Zava;
+
+-- Tablas de la base de datos Zava
+CREATE TABLE Roles (
+    id_rol INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion TEXT NOT NULL
+);
 
 CREATE TABLE Usuarios (
-    id_usuario PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     nickname VARCHAR(50) UNIQUE NOT NULL,
     correo VARCHAR(100) UNIQUE NOT NULL,
     telefono VARCHAR(15) NOT NULL,
     contrasenia VARCHAR(100) NOT NULL,
-    rol FOREIGN KEY REFERENCES Roles(id_rol),
-    foto VARCHAR(255) DEFAULT 'perfil.png',
-);
-
-CREATE TABLE Roles (
-    id_rol PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    descripcion TEXT NOT NULL
+    rol INT, FOREIGN KEY (rol) REFERENCES Roles(id_rol),
+    foto VARCHAR(255) DEFAULT 'perfil.png'
 );
 
 CREATE TABLE Usuarios_Baneados ( -- En caso de unban, se borra el registro
-    id_baneado PRIMARY KEY AUTO_INCREMENT,
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
-    nickname FOREIGN KEY REFERENCES Usuarios(nickname),
-    correo FOREIGN KEY REFERENCES Usuarios(correo),
+    id_baneado INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
+    nickname VARCHAR(50), FOREIGN KEY (nickname) REFERENCES Usuarios(nickname),
+    correo VARCHAR(100), FOREIGN KEY (correo) REFERENCES Usuarios(correo),
     motivo TEXT NOT NULL,
-    fecha_baneo DATE NOT NULL,
-)
+    fecha_baneo DATETIME NOT NULL
+);
 
 CREATE TABLE Productos (
-    id_producto PRIMARY KEY AUTO_INCREMENT,
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
     nombre VARCHAR(100) NOT NULL,
     descripcion TEXT NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
@@ -39,14 +41,14 @@ CREATE TABLE Productos (
 );
 
 CREATE TABLE Favoritos_Productos (
-    id_favorito PRIMARY KEY AUTO_INCREMENT,
-    id_producto FOREIGN KEY REFERENCES Productos(id_producto),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario)
+    id_favorito INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT, FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
 
 CREATE TABLE Recetas (
-    id_receta PRIMARY KEY AUTO_INCREMENT,
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
+    id_receta INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
     nombre VARCHAR(100) NOT NULL,
     descripcion TEXT NOT NULL,
     pasos TEXT NOT NULL,
@@ -60,27 +62,27 @@ CREATE TABLE Recetas (
 );
 
 CREATE TABLE Comentarios_Recetas (
-    id_comentario PRIMARY KEY AUTO_INCREMENT,
-    id_receta FOREIGN KEY REFERENCES Recetas(id_receta),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
+    id_comentario INT AUTO_INCREMENT PRIMARY KEY,
+    id_receta INT, FOREIGN KEY (id_receta) REFERENCES Recetas(id_receta),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
     comentario TEXT NOT NULL
 );
 
 CREATE TABLE Favoritos_Recetas (
-    id_favorito PRIMARY KEY AUTO_INCREMENT,
-    id_receta FOREIGN KEY REFERENCES Recetas(id_receta),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario)
+    id_favorito INT AUTO_INCREMENT PRIMARY KEY,
+    id_receta INT, FOREIGN KEY (id_receta) REFERENCES Recetas(id_receta),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
 
 CREATE TABLE Recetas_Hechas (
-    id_receta_hecha PRIMARY KEY AUTO_INCREMENT,
-    id_receta FOREIGN KEY REFERENCES Recetas(id_receta),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario)
+    id_receta_hecha INT AUTO_INCREMENT PRIMARY KEY,
+    id_receta INT, FOREIGN KEY (id_receta) REFERENCES Recetas(id_receta),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
 
 CREATE TABLE Restaurantes (
-    id_restaurante PRIMARY KEY AUTO_INCREMENT,
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
+    id_restaurante INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
     nombre VARCHAR(100) NOT NULL,
     promedio_calificacion DECIMAL(2, 1) NOT NULL,
     descripcion TEXT NOT NULL,
@@ -89,21 +91,21 @@ CREATE TABLE Restaurantes (
 );
 
 CREATE TABLE Comentarios_Restaurantes (
-    id_comentario PRIMARY KEY AUTO_INCREMENT,
-    id_restaurante FOREIGN KEY REFERENCES Restaurantes(id_restaurante),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
+    id_comentario INT AUTO_INCREMENT PRIMARY KEY,
+    id_restaurante INT, FOREIGN KEY (id_restaurante) REFERENCES Restaurantes(id_restaurante),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
     comentario TEXT NOT NULL
 );
 
 CREATE TABLE Reseñas_Restaurantes (
-    id_resena PRIMARY KEY AUTO_INCREMENT,
-    id_restaurante FOREIGN KEY REFERENCES Restaurantes(id_restaurante),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario),
-    calificacion DECIMAL(2, 1) NOT NULL,
+    id_resena INT AUTO_INCREMENT PRIMARY KEY,
+    id_restaurante INT, FOREIGN KEY (id_restaurante) REFERENCES Restaurantes(id_restaurante),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
+    calificacion DECIMAL(2, 1) NOT NULL
 );
 
 CREATE TABLE Favoritos_Restaurantes (
-    id_favorito PRIMARY KEY AUTO_INCREMENT,
-    id_restaurante FOREIGN KEY REFERENCES Restaurantes(id_restaurante),
-    id_usuario FOREIGN KEY REFERENCES Usuarios(id_usuario)
+    id_favorito INT AUTO_INCREMENT PRIMARY KEY,
+    id_restaurante INT, FOREIGN KEY (id_restaurante) REFERENCES Restaurantes(id_restaurante),
+    id_usuario INT, FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
